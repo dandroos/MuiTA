a10ec3de40a0b9992c51781d17bb8c5ec118fdb8
David Andrews <dandrewsuk82@gmail.com>
2020-08-10 16:29:20 +0100

    Updates

---
+++ precache-manifest.64a55ad334fd5ef7786a0e861b3b210b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef4897719aaa1e7fe3a2354af9e1ec9",
+    "url": "/muita/index.html"
+  },
+  {
+    "revision": "93f595e025b83c089ec5",
+    "url": "/muita/static/js/2.56e03c12.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/muita/static/js/2.56e03c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29586beea4cc77f7fa45",
+    "url": "/muita/static/js/main.dfda06d0.chunk.js"
+  },
+  {
+    "revision": "cf60a5b06916ce90b4ec",
+    "url": "/muita/static/js/runtime-main.8540636f.js"
+  }
+]);
